84070f3255e514fe02486950524c0ca2f9b04e3f
daw2-casado20 <55798413+daw2-casado20@users.noreply.github.com>
2020-04-25 18:18:10 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
-$db = mysqli_connect('localhost', 'adri', 'root') or 
+$db = mysqli_connect('localhost', 'id13394599_administrator', '->f9qDQwGDM7Fqq=') or 
     die ('Unable to connect. Check your connection parameters.');
-mysqli_select_db($db, 'emociones') or die(mysqli_error($db));
+mysqli_select_db($db, 'id13394599_usuarios') or die(mysqli_error($db));
 ?>
 <html>
  <head>
@@ -15,10 +15,10 @@ mysqli_select_db($db, 'emociones') or die(mysqli_error($db));
  <body>
  <table style="width:100%;">
   <tr>
-   <th colspan="2">Usuarios registrados<a href="music.php?action=add">[ADD]</a></th>
+   <th colspan="2">Usuarios registrados<a href="music.php?action=add">[AGREGAR USER]</a></th>
   </tr>
 <?php
-$query = 'SELECT * FROM users';
+$query = 'SELECT * FROM usuarios';
 $result = mysqli_query($db, $query) or die (mysqli_error($db));
 $odd = true;
 while ($row = mysqli_fetch_assoc($result)) {
@@ -27,16 +27,16 @@ while ($row = mysqli_fetch_assoc($result)) {
     echo '<td style="width:75%;">'; 
     echo $row['nombre'];
     echo '</td><td>';
-    echo ' <a href="music.php?action=edit&id=' . $row['id_alumno'] . '"> [EDIT]</a>'; 
-    echo ' <a href="delete.php?type=movie&id=' . $row['id_alumno'] . '"> [DELETE]</a>';
+    echo ' <a href="music.php?action=edit&id=' . $row['id_alumno'] . '"> [EDITAR USUARIO]</a>'; 
+    echo ' <a href="delete.php?type=movie&id=' . $row['id_alumno'] . '"> [BORAR USUARIO]</a>';
     echo '</td></tr>';
 }
 ?>
   <tr>
-    <th colspan="2">Emociones registradas<a href="people.php?action=add"> [ADD]</a></th>
+    <th colspan="2">Emociones registradas<a href="people.php?action=add"> [AGREGAR EMOCIÓN]</a></th>
   </tr>
 <?php
-$query = 'SELECT * FROM emocion';
+$query = 'SELECT * FROM emociones';
 $result = mysqli_query($db, $query) or die (mysqli_error($db));
 $odd = true;
 while ($row = mysqli_fetch_assoc($result)) {
@@ -46,9 +46,9 @@ while ($row = mysqli_fetch_assoc($result)) {
     echo $row['comentario'];
     echo '</td><td>';
     echo ' <a href="people.php?action=edit&id=' . $row['id_emocion'] .
-        '"> [EDIT]</a>'; 
+        '"> [EDITAR EMOCIÓN]</a>'; 
     echo ' <a href="delete.php?type=people&id=' . $row['id_emocion'] .
-        '"> [DELETE]</a>';
+        '"> [BORRAR EMOCIÓN]</a>';
     echo '</td></tr>';
 }
 ?>
