65c215b550f2197b03aa977822eec5d6b00c27a1
daw2-casado20 <55798413+daw2-casado20@users.noreply.github.com>
2020-04-25 18:20:18 +0200

    Update people.php

--- people.php
+++ people.php
@@ -1,13 +1,13 @@
 <?php
-$db = mysqli_connect('localhost', 'adri', 'root') or 
+$db = mysqli_connect('localhost', 'id13394599_administrator', '->f9qDQwGDM7Fqq=') or 
     die ('Unable to connect. Check your connection parameters.');
-mysqli_select_db($db, 'emociones') or die(mysqli_error($db));
+mysqli_select_db($db, 'id13394599_usuarios') or die(mysqli_error($db));
 if ($_GET['action'] == 'edit') {
     //retrieve the record's information 
     $query = 'SELECT
-            id_alumno, codigoEmocional, comentario, fecha, hora
+            id_alumno, codigo_emocional, comentario, fecha, hora
         FROM
-            emocion
+            emociones
         WHERE
             id_emocion = ' . $_GET['id'];
     $result = mysqli_query($db, $query) or die(mysqli_error($db));
@@ -15,16 +15,16 @@ if ($_GET['action'] == 'edit') {
 } else {
     //set values to blank
     $id_alumno = 0;
-    $codigoEmocional = 0;
+    $codigo_emocional = 0;
     $comentario = "";
     $fecha = date('Y-m-d');
-    $hora = time('H:i:s');
+    $hora = time('H-m-s');
     ;
 }
 ?>
 <html>
  <head>
-  <title><?php echo ucfirst($_GET['action']); ?> Musica</title>
+  <title><?php echo ucfirst($_GET['action']); ?> Emociones</title>
  </head>
  <body>
   <?php
@@ -41,8 +41,8 @@ if ($_GET['action'] == 'edit') {
       value="<?php echo $id_alumno; ?>"/></td>
     </tr><tr>
      <td>Codigo emocional del alumno</td>
-     <td><input type="number" name="codigoEmocional"
-      value="<?php echo $codigoEmocional; ?>"/></td>
+     <td><input type="number" name="codigo_emocional"
+      value="<?php echo $codigo_emocional; ?>"/></td>
     </tr>
     <tr>
      <td>Comentario</td>
